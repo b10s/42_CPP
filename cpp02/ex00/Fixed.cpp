6758f328f1e24f1a38eb0dd8fd951c2be71731c0
Enshin Andrei <b1os@bk.ru>
2025-08-27 21:37:03 +0900

    cpp02 ex00

---
+++ cpp02/ex00/Fixed.cpp
@@ -0,0 +1,39 @@
+#include "./Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed() {
+	std::cout << "Default constructor called" << std::endl;
+	this->value = 0;
+}
+
+Fixed::Fixed(const Fixed& other) {
+	std::cout << "Copy constructor called" << std::endl;
+	// Q: how we can call here copy assignment operator?
+	this->value = other.getRawBits();
+}
+
+Fixed& Fixed::operator=(const Fixed& other) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	// check for self assignment e.g. obj = obj
+	// here it is not needed but good to know
+	if (this == &other) {
+		return *this;
+	}
+	this->value = other.getRawBits();
+	return *this;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->value;
+}
+
+void Fixed::setRawBits(int const raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->value = raw;
+}
+
---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,30 @@
+/* Orthodox Canonical Form
+		- default constructor
+		- copy constructor
+		- copy assignment operator
+		- destructor
+
+		rule of three (default constructor is not counted I guess):
+		https://en.cppreference.com/w/cpp/language/rule_of_three.html
+*/
+
+class Fixed {
+	private:
+		int value;
+		// When applied to a member variable of a class, static means
+		// there is only one copy of that variable,
+		// shared across all instances of the class.
+		static const int fraction = 8;
+	public:
+		Fixed();
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& other);
+		~Fixed();
+		// constant member function:
+		// 	- cannot modify any non-static member variables of the class object on which it is called
+		//	- cannot call any non-const member functions of the same class object, as those functions might modify the object's state
+		//	- can be called on both const and non-const objects of the class
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+};
+
