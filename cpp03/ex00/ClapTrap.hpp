2b284ed6fbb0413a160534559869d716c5bb1dde
Andrei Enshin <aenshin@c3r2s5.42tokyo.jp>
2025-09-06 01:02:12 +0900

    fin ex00

--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -1,5 +1,33 @@
+// ClapTrap is a character featured in the action role-playing game series Borderlands.
+// ref.: https://en.wikipedia.org/wiki/Claptrap
 
+#include <string>
 //TODO: header protection
 
+//TODO: read and document here why copy constructor and assign operator is needed
+// what is benefits of orthodox canon form
+
+//TODO: if I have constructor with arg name, do I need to have constractor without args?
+// check cppref for rule of four
+
+//Q: why constructors have not return value? no void no ClassName?
 class ClapTrap {
+	public:
+		ClapTrap(const std::string& name);
+
+		ClapTrap();
+		ClapTrap(const ClapTrap& other);
+		ClapTrap& operator=(const ClapTrap& other);
+		~ClapTrap();
+
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+			
+	private:
+		std::string name;
+		unsigned int hitPoinst; // i.e. health of ClapTrap
+		unsigned int energyPoints;
+		unsigned int attackDamage;
 };
+
--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -1,6 +1,45 @@
 #include "./ClapTrap.hpp"
+#include <iostream>
 
 int main() {
-	// ..
+	ClapTrap ct0 = ClapTrap();
+	ClapTrap ct1 = ClapTrap("Interplanetary Ninja Assassin");
+	ClapTrap ct2 = ClapTrap("Idiot");
+
+
+	std::cout << "test starts" << std::endl;
+
+	ct0.attack("Wheatley");
+	ct0.attack("Wheatley");
+	ct0.attack("Wheatley");
+	ct0.attack("Wheatley");
+	ct0.attack("Wheatley");
+	ct0.attack("Wheatley");
+	ct0.attack("Wheatley");
+	ct0.attack("Wheatley");
+	ct0.attack("Wheatley");
+	ct0.attack("Wheatley");
+	ct0.attack("Wheatley");
+
+	ct1.takeDamage(1);
+	ct1.takeDamage(42);
+	ct1.takeDamage(100);
+
+	ct2.beRepaired(1);
+	ct2.beRepaired(1);
+	ct2.beRepaired(1);
+	ct2.beRepaired(1);
+	ct2.beRepaired(1);
+	ct2.beRepaired(1);
+	ct2.beRepaired(1);
+	ct2.beRepaired(1);
+	ct2.beRepaired(1);
+	ct2.beRepaired(1);
+	ct2.beRepaired(1);
+
+
+	std::cout << "test ends" << std::endl;
+
+
 	return 0;
 }
